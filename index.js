9c90d820b504028122573e92854011d80e05e295
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2021-11-26 04:24:20 -0800

    update::submit

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ console.log("In This Project We are using Regular expression in JS ")
 //Geeting The document Readt
 let username=document.getElementById("username")
 let Email=document.getElementById("Email")
-let Phone=document.getElementById("Phone")
+let Phone=document.getElementById("Phone");
+
+validEmail=false;
+validPhone=false;
+validusername=false;
+
+
 
 //console.log(username,Email,Phone)
 
@@ -14,9 +20,12 @@ username.addEventListener("blur",()=>{
     let string=username.value;
     if(regex.test(string)){
         console.log("This Validate This Email")
+        validusername=true;
+
     }
     else{
         console.log("This Does Not validate The Email")
+        validusername=false;
     }
 })
 
@@ -27,9 +36,13 @@ Email.addEventListener("blur",()=>{
     let string=Email.value;
     if(regex.test(string)){
         console.log("You have validate the Email ")
+        validEmail=true;
+
     }
     else{
-        console.log("You Have Not Validate the EMail ")
+        console.log("You Have Not Validate the EMail")
+        validEmail=false;
+
     }
 
 })
@@ -41,10 +54,47 @@ Phone.addEventListener("blur",()=>{
     let string=Phone.value;
     if(regex.test(string)){
         console.log("You Have Validate The Phone ")
+        validPhone=true;
+
     }
     else{
         console.log("You Does Not have validate the Phone")
+        validPhone=false;
+
     }
+
+
+})
+
+//Submiitinf the
+let buttonid=document.getElementById("buttonid")
+buttonid.addEventListener("click",(e)=>{
+    e.preventDefault();
+
+    //Submittinf the form
+    //now the True Event
+    if(validEmail && validPhone && validusername){
+        //for checking
+        console.log(validPhone,validusername,validEmail)
+        console.log("Submitted Valid Details")
+        let Success=document.getElementById("Success")
+         Success.classList.add("show")
+         Failure.classList.remove("show")
+    }
+    else{
+        console.log("Error Submitted Wrong Details")
+        let Failure=document.getElementById("Failure")
+        Failure.classList.add("show")
+        Success.classList.remove("show")
+    }
+
+     //console.log("Your Form Has Been Succssfully Submited")
+    
+
+
+
+ 
+
 
 
 })
